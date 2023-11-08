dc03a806296557e400fd8d8ed797cfdf0e119954
lakshmilokeshhema <149066889+lakshmilokeshhema@users.noreply.github.com>
2023-11-08 09:44:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "uday-s3-bucket-123456789"
+  bucket = "uday-s3-bucket-1234567891"
   tags = {
     Name        = "uday-s3-bucket-123456789"
     Environment = "Dev"
+    Workspace="dev"
   }
   
 }
